63e20a7e3ab3575f8b04616288c14b6b6a436489
abhay-rana <abhayrana286@gmail.com>
2023-05-03 15:56:19 +0530

    eslint if the path is not defined

--- src/screens/about-screen.js
+++ src/screens/about-screen.js
@@ -1,9 +1,12 @@
 import React, { memo } from 'react';
 
+import CompOne from '~/components/comp-one';
+
 const AboutScreen = () => {
     return (
         <>
             <div className="">AboutScreen</div>
+            <CompOne />
         </>
     );
 };
