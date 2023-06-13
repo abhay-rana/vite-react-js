e6a47aab0337d3acb68a5d3af49e65f575548480
abhay-rana <abhayrana286@gmail.com>
2023-06-13 23:31:12 +0530

    remove console

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import fs from 'fs/promises';
 import path from 'path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import removeConsole from 'vite-plugin-remove-console';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
@@ -15,6 +16,7 @@ export default defineConfig(() => ({
             exportAsDefault: true,
         }),
         eslint(),
+        removeConsole(),
     ],
     server: {
         // open at the port
