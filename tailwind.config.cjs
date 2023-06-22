3290a785f49daa6411e3cb3258fbf8abf9d2bb48
abhay-rana <abhayrana286@gmail.com>
2023-06-22 23:58:20 +0530

    tailwind configurations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,37 @@
 /** @type {import('tailwindcss').Config} */
-const colors=require("./src/styles/theme-var.js")
+const colors = require('./src/styles/theme-var.js');
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,mdx}'],
     darkMode: 'class', //dark mode is apply when there is change in the root class
     theme: {
         fontFamily: {
-            custom: ['Poppins', 'sans-serif'], //font links should be at the index.html scripts
+            // add the font-size to 62.%% in .css file in html tag
+            poppins: ['Poppins', 'sans-serif'], //font links should be at the index.html scripts
+            custom_font_name: ['ADELIA', 'cursive'],
+            // font-poppins , font-adelia to use the custom fonts classes
+        },
+        fontSize: {
+            12: '1.2rem', // 12px
+            14: '1.4rem', // 14px
+            16: '1.6rem', // 16px
+            18: '1.8rem', // 18px
+            20: '2.0rem', // 20px
+            24: '2.4rem', // 24px
+            28: '2.8rem', //28px
+            32: '3.2rem', // 32px
+            48: '4.8rem', // 48px
+            56: '5.6rem', // 56px
+            64: '6.4rem', // 64px
         },
         extend: {
+            // screen responsive break-points
+            screens: {
+                '@sm': { max: '639px' },
+                '@md': { min: '640px', max: '1023px' },
+                '@lg': { min: '1024px', max: '1440px' },
+            },
             colors: colors,
             padding: {
                 5: '1rem',
@@ -20,17 +44,5 @@ module.exports = {
             },
         },
     },
-    // safelist: [
-    //     // https://tailwindcss.com/docs/content-configuration#safelisting-classes
-    //     // https://github.com/tailwindlabs/tailwindcss/discussions/10079
-    //     // safelist pattern only for the storybook dynamic changing from the website
-    //     // for dynamic  styling it is best to use the style prop of css
-    //     {
-    //         pattern: /(bg|border|text)-[^/]+$/,
-    //         variants: ['dark', 'hover', 'focus', 'dark:hover', 'dark:focus'],
-    //     },
-    //     { pattern: /^from/ },
-    //     { pattern: /^to/ },
-    // ],
     plugins: [],
 };
