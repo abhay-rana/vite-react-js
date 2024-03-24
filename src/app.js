ff6537f4b5c6be517ba2f569fdc239827072b13d
abhay-rana <abhayrana286@gmail.com>
2024-03-24 14:46:32 +0530

    routing handles

--- src/app.js
+++ src/app.js
@@ -16,3 +16,39 @@ const App = () => {
 };
 
 export default memo(App);
+
+// const App = () => {
+//     const [location, setLocation] = useLocation();
+//     const store = useAppSelector((state) => ({
+//         is_login: state.auth_store.is_login,
+//     }));
+
+//     useEffect(() => {
+//         if (!store.is_login) {
+//             setLocation('/login');
+//         } else if (store.is_login && AUTH_ROUTES.includes(location)) {
+//             setLocation('/home');
+//         } else {
+//             setLocation('/home');
+//         }
+//     }, [store.is_login]);
+
+//     return (
+//         <>
+//             <Container is_login={store.is_login}>
+//                 <Router>
+//                     <Switch>
+//                         {/* Two Stacks Based on the user authentications */}
+//                         {!store.is_login ? <AuthRoutes /> : <MainRoutes />}
+//                         {/* Common Routes */}
+//                         <Route path="/login" component={'LoginScreen'} />
+//                         {/* if none of the route is matched */}
+//                         <Route component={ErrorScreen} />
+//                     </Switch>
+//                 </Router>
+//             </Container>
+//         </>
+//     );
+// };
+
+// export default App;
