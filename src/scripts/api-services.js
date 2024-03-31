a58fd7d9753b754599057e1d9cabac8910f27565
abhay-rana <abhayrana286@gmail.com>
2024-03-31 10:27:00 +0530

    api services

--- src/scripts/api-services.js
+++ src/scripts/api-services.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
+import { Logout } from '~/reducers/container-reducer';
 import store from '~/reducers/store';
 
 import { ProjectUrl } from '~/env';
-
-const api = axios.create({
-    baseURL: ProjectUrl,
-});
 
 //* 2xx ->  Success
 //* 4xx ->  client side error
@@ -18,6 +15,15 @@ const api = axios.create({
 //! 404 Not Found    -> endpoint does not exist
 //! 500 Internal Server Error  -> error from the server side
 
+const api = axios.create({
+    baseURL: ProjectUrl,
+    headers: {
+        'Content-Type': 'application/json',
+        // Add any other headers or configurations you need
+    },
+});
+
+//! Add a request interceptor
 api.interceptors.request.use(
     async (config) => {
         let token = store.getState().auth_store.token;
@@ -32,49 +38,82 @@ api.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
+const handleLogout = () => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+        store.dispatch(Logout());
+        // Clear any other relevant data upon logout
+        localStorage.removeItem('token');
+        localStorage.removeItem('refresh_token');
+    }
+};
+
+const handleApiError = (error) => {
+    if (
+        error.response &&
+        (error.response.status === 401 || error.response.status === 403)
+    ) {
+        handleLogout();
+        return Promise.reject(error.response.data);
+    }
+    console.log('error', error.response.data);
+    // Handle other errors as needed
+    return Promise.reject(error.response.data);
+};
+
+api.interceptors.response.use(
+    (response) => response,
+    async function (error) {
+        const originalRequest = error.config;
+
+        if (error.response.status === 403 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+                const refresh_token = localStorage.getItem('refresh_token');
+                const response = await axios.get(`${ProjectUrl}/refresh`, {
+                    headers: { Authorization: `Bearer ${refresh_token}` },
+                });
+
+                const { token } = response.data;
+                localStorage.setItem('token', token);
+
+                // Retry the original request with the new token
+                originalRequest.headers.Authorization = `Bearer ${token}`;
+                return api(originalRequest);
+            } catch (error) {
+                handleLogout();
+            }
+        } else if (
+            error.response.status === 401 ||
+            error.response.status === 403
+        ) {
+            handleLogout();
+        }
+
+        return Promise.reject(error);
+    }
+);
+
 export const postApi = async (
     path,
     data = {},
     headers = { Accept: 'application/json' }
 ) => {
-    var result = await new Promise((resolve, reject) => {
-        api.post(path, data, {
-            headers: headers,
-        })
-            .then((response) => {
-                console.log(response);
-                return resolve(response);
-            })
-            .catch((error) => {
-                //! NOTE - use "error.response.data` (not "error")
-                console.log('hey post api error');
-                console.log(error.response.data);
-                // if (error.response.status === 401) {
-                //     store.getState().auth.email &&
-                //         store.dispatch(userForceSignOut());
-                //     return;
-                // }
-                return reject(error.response.data);
-            });
-        api.get;
-    });
-    return result;
+    try {
+        const response = await api.post(path, data, { headers });
+        return response;
+    } catch (error) {
+        return handleApiError(error);
+    }
 };
 
-export const getApi = async (path, data = {}) => {
-    var result = await new Promise((resolve, reject) => {
-        api.get(path, data)
-            .then((response) => resolve(response.data))
-            .catch((error) => {
-                // if (error.response.status === 401) {
-                //     store.getState().auth.email &&
-                //         store.dispatch(userForceSignOut());
-                //     return;
-                // }
-
-                return reject(error.response.data);
-            });
-        api.get;
-    });
-    return result;
+export const getApi = async (path) => {
+    try {
+        const response = await api.get(path);
+        return response.data;
+    } catch (error) {
+        return handleApiError(error);
+    }
 };
